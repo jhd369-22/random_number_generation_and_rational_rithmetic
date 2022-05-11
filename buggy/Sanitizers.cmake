73870ec80002e96f6c1b292ce9c58aacd49c7cd9
Junhong Deng <dengjunhong@gmail.com>
2022-05-10 17:24:58 -0700

    Q4

---
+++ buggy/Sanitizers.cmake
@@ -0,0 +1,14 @@
+# define options for enabling the Address Sanitizer(ASan) and
+# the Undefined Behavior Sanitizer(UBSan)
+option(ENABLE_ASAN false)
+option(ENABLE_UBSAN false)
+
+if(ENABLE_ASAN)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address")
+endif()
+
+if(ENABLE_UBSAN)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=undefined")
+endif()
