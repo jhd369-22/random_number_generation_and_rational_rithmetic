3f1e3fe935424ddf3f73fdab384ff868ad4f8757
Junhong Deng <dengjunhong@gmail.com>
2022-05-23 08:53:11 -0700

    rename README.pdf

--- app/test_random.cpp
+++ app/test_random.cpp
@@ -3,4 +3,34 @@
 #include <include/ra/random.hpp>
 
 
+// catch2 test cases
+TEST_CASE("ra::random::linear_congruential_generator::default_seed", "[ra::random::linear_congruential_generator]") {
+    ra::random::linear_congruential_generator lcg(123, 456, 789);
+    REQUIRE(lcg.seed() == ra::random::linear_congruential_generator::default_seed());
+}
 
+TEST_CASE("ra::random::linear_congruential_generator::seed", "[ra::random::linear_congruential_generator]") {
+    ra::random::linear_congruential_generator lcg(123, 456, 789);
+    lcg.seed(123);
+    REQUIRE(lcg.seed() == 123);
+}
+
+TEST_CASE("ra::random::linear_congruential_generator::operator()", "[ra::random::linear_congruential_generator]") {
+    ra::random::linear_congruential_generator lcg(123, 456, 789);
+    lcg.seed(123);
+    for (int i = 0; i < 10; ++i) {
+        REQUIRE(lcg() == i);
+    }
+}
+
+TEST_CASE("ra::random::linear_congruential_generator::operator==", "[ra::random::linear_congruential_generator]") {
+    ra::random::linear_congruential_generator lcg(123, 456, 789);
+    ra::random::linear_congruential_generator lcg2(123, 456, 789);
+    REQUIRE(lcg == lcg2);
+    lcg.seed(123);
+    REQUIRE(lcg != lcg2);
+}
+
+TEST_CASE("ra::random::linear_congruential_generator::operator!=", "[ra::random::linear_congruential_generator]") {
+    ra::random::linear_congruential_generator lcg(123, 456, 789);
+    ra::random::linear
